8baf488a53d172e74ce8682bea7339c017996917
ArthurSMA <arthursuassunamaiaalves@gmail.com>
2023-10-29 09:53:30 -0300

    master: adicionando lib cors para que seja chamado no front

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require("express");
-const app = express();
+const cors = require("cors");
+
 const port = 9090;
+const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 const usuarioRoutes = require("./src/routes/usuarioRoutes");
 app.use("/api/usuarios", usuarioRoutes);
